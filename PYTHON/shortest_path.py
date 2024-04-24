b5faf7c6e0ccf6239e5f46f84c5d89d1b4ec9c0e
Pinerealm <osalami09@gmail.com>
2024-04-24 08:20:06 +0300

    Refactor shortest_path function

--- PYTHON/shortest_path.py
+++ PYTHON/shortest_path.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-
-my_graph = {
-    'A': [('B', 3), ('D', 1)],
-    'B': [('A', 3), ('C', 4)],
-    'C': [('B', 4), ('D', 7)],
-    'D': [('A', 1), ('C', 7)]
-}
+"""Find the shortest path in a graph"""
 
 
 def shortest_path(graph, start):
@@ -34,4 +28,10 @@ def shortest_path(graph, start):
 
 
 if __name__ == '__main__':
+    my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+    }
     shortest_path(my_graph, 'A')
